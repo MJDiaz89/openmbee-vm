e52f7a777fbd4c7975f7e320d6dd330a0f883cf0
Diaz, Manuel Joel <ManuelJDiaz@gatech.edu>
2020-02-04 15:24:18 -0500

    more local sh file tweaks

--- new_local_install.sh
+++ new_local_install.sh
@@ -2,9 +2,12 @@ echo ">>> removing old docker containers";
 docker rm -f v342-mms v342-elastic v342-activemq v342-solr v342-postgres;
 
 echo ">>> docker-compose up..."
-docker-compose up > docker-compose-logs.txt;
-echo ">>> docker-compose done "
+docker-compose up -d;
+docker-compose logs --no-color > docker-compose-logs.txt;
+echo ">>> docker-compose done. Logs saved to 'docker-compose-logs.txt'"
 
+
+# ========= Postgres =========
 # need to create `postgres` user
 if ! `docker exec -T postgresql psql -U mms postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='postgres'" | grep -q "1"`; then
     echo "  > Creating 'postgres' user";
@@ -19,7 +22,9 @@ else
     echo "  > User 'postgres' already exists";
 fi
 
-# fix bad permissions
+
+# ========= Elasticsearch =========
+# fix bad  permissions
 echo ">>> Fixing bad permissions in Elasticsearch";
 docker exec -it --priviledged=true -u root v342-elastic sh -c "chown -R elasticresearch:elasticresearch /tmp/elasticresearch";
 docker exec -it --priviledged=true -u root v342-elastic sh -c "chown -R elasticresearch:elasticresearch /var/data";
@@ -28,11 +33,15 @@ docker exec -it --priviledged=true -u root v342-elastic sh -c "chown -R elasticr
 echo ">>> Fixing bad permissions in Elasticsearch";
 docker exec -it --priviledged=true -u root v342-elastic sh -c "mms-mappings.sh" < /vagrant/mms-mappings.sh;
 
-#restart
-echo ">>> restarting Elasticsearch";
-docker restart v342-elastic;
 
+# ========= MMS =========
 echo ">>> copy correct config files to vagrant vm...";
 docker exec -i v342-mms sh -c "cat > /usr/local/tomcat/shared/classes/alfresco-global.properties" < alfresco-global.properties;
 docker exec -i v342-mms sh -c "cat > /usr/local/tomcat/shared/classes/mms.properties" < mms.properties;
 docker exec -i v342-mms sh -c "cat > /usr/local/tomcat/conf/tomcat-users.xml" < tomcat-users.xml;
+
+
+# ========= clean up =========
+echo ">>> restarting all containers...";
+docker restart  v342-elastic v342-activemq v342-solr v342-postgres v342-mms;
+echo ">>> all containers restarted";
